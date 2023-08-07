0e021144fa10c45adfa8caff5ae2d9fc30ea10eb
Roohan Khan <roohank8@gmail.com>
2023-08-08 03:47:15 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import NotesListPage from "./pages/NotesListPage";
-import NotePage from './pages/NotePage'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotePage from "./pages/NotePage";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="container dark">
-        <div className='app'>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<NotesListPage/>} />
-          <Route path="/note/:id" element={<NotePage/>} />
-        </Routes>
-      </div></div>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<NotesListPage />} />
+            <Route path="/note/:id" element={<NotePage />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
